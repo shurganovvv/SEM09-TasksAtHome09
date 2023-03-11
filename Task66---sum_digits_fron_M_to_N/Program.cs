d9329e3f15b3619a7cd1cdbc78de948db561b19d
viktor <shurganovvv@yandex.ru>
2023-03-12 00:12:05 +0300

    add TasksAtHome09 project

---
+++ Task66---sum_digits_fron_M_to_N/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая 
+//     найдёт сумму натуральных элементов в промежутке от M до N.
+//         M = 1; N = 15 -> 120
+//         M = 4; N = 8. -> 30
+
+int SumDigits(int m, int n)
+{
+    if (m > n) return 0;
+    return m + SumDigits(m + 1, n);
+    // return n + SumDigits(m, n-1);
+}
+
+System.Console.Write("Введите два числа через ПОБЕЛ: ");
+int[] lazyWrite = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int M = lazyWrite[0];
+int N = lazyWrite[1];
+if (M > N)
+    System.Console.WriteLine(SumDigits(N, M));
+System.Console.WriteLine(SumDigits(M, N));
---
+++ Task68---Ackermann_fun/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 68: 
+//     Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//     Даны два неотрицательных числа m и n. 
+//         m = 2, n = 3 -> A(m,n) = 9
+
+int WilhelmFriedrichAckermann(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return WilhelmFriedrichAckermann(m - 1, 1);
+    else return WilhelmFriedrichAckermann(m - 1, WilhelmFriedrichAckermann(m, n - 1));
+}
+
+System.Console.Write("Введите два числа через ПОБЕЛ: ");
+int[] lazyWrite = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int Péter = Math.Abs(lazyWrite[0]);
+int Rózsa = Math.Abs(lazyWrite[1]);
+System.Console.WriteLine(WilhelmFriedrichAckermann(Péter, Rózsa));
+
+
+// кроме того, нашёл вариант решения, пока не понимаю, но оставил себе на разобраться:
+System.Console.WriteLine(ack(Péter, Rózsa));
+
+int ack(int n, int m) {
+    while (n != 0) {
+        if (m == 0)
+            m = 1;
+        else
+            m = ack(n, m - 1);
+        n = n - 1;
+    }
+    return m + 1;
+}
